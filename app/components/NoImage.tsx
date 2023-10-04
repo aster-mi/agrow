8742a25ae81f076e69f4e27ca946ddef48711e02
aster-mi <aster.micro.829@gmail.com>
2023-10-04 21:21:16 +0900

    fix image

--- app/components/NoImage.tsx
+++ app/components/NoImage.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
 import localImage from "@/public/noimage.jpg";
 export default function NoImage() {
-  return <Image src={localImage} width={200} height={200} alt="no image" />;
+  return (
+    <Image
+      src={localImage}
+      priority={true}
+      width={500}
+      height={500}
+      alt="no image"
+    />
+  );
 }
